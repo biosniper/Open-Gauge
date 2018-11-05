b9f107f46c1d72066ae19ca3d09e8d49074b685c
biosniper <biosniper@gmail.com>
2018-11-05 21:05:51 +0000

    Update Open-Gauge.ino
    v0.06
    Added fake boost pressure calculated from barometric for layout testing.
    Changed temp and altitude precision to neaten layout and stop irritating number changes due to display updates.

--- Open-Gauge.ino
+++ Open-Gauge.ino
@@ -1,15 +1,14 @@
 /*
- * Open-Gauge v0.05
+ * Open-Gauge v0.06
  * This is very much in development and right now is just testing layout and code to get it about right
  * Once this is done we will start using real values from a pressure sensor
  */
 
 int mapsen = 0;
-float boost = 0.1;
-float mapsenval = 0.1;
-
+float boost = 0;
+float mapsenval = 0;
 float warnpsi = 20.5;
-float baropress = 1;
+float baropress = 0;
 
 #include <Wire.h>
 #include <Adafruit_GFX.h>
@@ -30,7 +29,7 @@ void setup() {
   display.setTextColor(WHITE);
   display.setCursor(0,20);
   display.print(F("Open-Gauge"));
-  display.println(F("v0.05"));
+  display.println(F("v0.06"));
   display.display();
   delay(2000);
   
@@ -51,27 +50,28 @@ void setup() {
   display.setCursor(0,10);
   display.print(F("BMP280 = OK"));
   display.display();
-  delay(2000);
+  delay(1000);
   display.clearDisplay();
 }
 
 void loop() {
+  boost = ((float)bme.readPressure()*0.0001450377); // Use barometric pressure to PSI to create fake but live PSI for layout testing
   display.drawRect(0,0, 128, 64, WHITE); // Location, Size, Colour
   display.drawFastHLine(0,10, 128, WHITE); // Location, Length, Colour
   display.drawFastVLine(64,0, 10, WHITE);
   display.setTextSize(1);
   display.setCursor(12,2);
-  display.print(bme.readTemperature());
+  display.print(bme.readTemperature(),1);
   display.print(F("*C"));
   display.setCursor(72,2);
-  display.print(bme.readAltitude(1013.25)); // Maybe replace altimiter with voltage readout because how usefil is an altimiter anyway?
+  display.print(bme.readAltitude(1013.25),0); // Maybe replace altimiter with voltage readout because how useful is an altimiter anyway?
   display.setCursor(120,2);
   display.print(F("m"));
   display.setCursor(12,16);
   display.setTextSize(2);
   display.print(F("PSI")); // Example display layout untill pressure sensor is wired in
-  display.setCursor(69,16);
-  display.print(F("34.1"));
+  display.setCursor(67,16);
+  display.print(boost,1);
   display.fillRect(12,35, 5,10, WHITE); // Location, Size, Colour
   display.fillRect(20,35, 5,10, WHITE);
   display.fillRect(28,35, 5,10, WHITE);
